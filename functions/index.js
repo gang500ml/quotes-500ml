5245e1f694d50289cc62fd3f57e1935ef2e1f201
chompoo500ml <chompoo@gang500.ml>
2018-04-13 15:13:48 +0000

    add quote id

--- functions/index.js
+++ functions/index.js
@@ -2,27 +2,16 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
-exports.addQuote = functions.https.onRequest((req, res) => {
-  const quote = req.query.quote;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  return admin.database().ref('/candidates').push({quote: quote}).then((snapshot) => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    return res.redirect(303, snapshot.ref.toString());
-  });
+exports.getQuote = functions.https.onRequest((req, res) => {
+    const quote = req.query.quote;
+    return admin.database().ref('/candidates').push({id: '0', quote: quote}).then((snapshot) => {
+        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+        return res.redirect(303, snapshot.ref.toString());
+    });
 });
 
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+exports.updateQuoteId = functions.database.ref('/quotes/{pushId}/quote')
     .onCreate((snapshot, context) => {
       // Grab the current value of what was written to the Realtime Database.
-      const original = snapshot.val();
-      console.log('Uppercasing', context.params.pushId, original);
-      const uppercase = original.toUpperCase();
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to the Firebase Realtime Database.
-      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-      return snapshot.ref.parent.child('uppercase').set(uppercase);
+      return snapshot.ref.parent.child('id').set(snapshot.numChildren());
     });
